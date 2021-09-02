c63b505aeba024bd31d31cdc1502200b0fd82e7c
aletsire <aletsire@gmail.com>
2021-09-02 13:19:45 +0900

    0902_TIL_Django_proto

---
+++ 4.Django/0902/articles/views.py
@@ -0,0 +1,63 @@
+from django.shortcuts import redirect, render
+from .models import Article
+
+# Create your views here.
+def index(request):
+    # articles = Article.objects.all()
+    # articles = Article.objects.all()[::-1] # 파이썬이 변경
+    articles = Article.objects.order_by('-id') # orm으로 순서 변경
+    context = {
+        'articles': articles
+    }
+    return render(request, 'articles/index.html', context)
+
+def new(request):
+    return render(request, 'articles/new.html')
+
+def create(request):
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+
+    # article = Article()
+    # article.title = title
+    # article.content = content
+    # article.save()
+
+    article = Article(title=title, content=content) # 앞은 model에서 지정한 값, 뒤는 위에서 정의 해준 값
+    article.save()
+
+    return redirect('articles:detail', article.pk)
+
+
+def detail(request,pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article
+    }
+
+    return render(request, 'articles/detail.html', context)
+
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    if request.method == 'POST':
+        article.delete()
+        return redirect('articles:index')
+    else:
+        return redirect('articles:detail', article.pk)
+    
+
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article
+    }
+
+    return render(request, 'articles/edit.html', context)
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.title = request.POST.get('title')
+    article.content = request.POST.get('content')
+    article.save()
+
+    return redirect('articles:detail', article.pk)
