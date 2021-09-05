2dbaf93169efa2193ed4f94e539c4e071cf34f48
aletsire <aletsire@gmail.com>
2021-09-05 21:39:30 +0900

    0905_TIL_Django_review_rejection

--- 4.Django/0905/articles/views.py
+++ 4.Django/0905/articles/views.py
@@ -28,3 +28,27 @@ def detail(request, pk):
     }
 
     return render(request, 'articles/detail.html', context)
+
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article':article
+    }
+
+    return render(request, 'articles/edit.html', context)
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.title = request.POST.get('title')
+    article.content = request.POST.get('content')
+    article.save()
+
+    return redirect('articles:detail', article.pk)
+
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    if request.method == 'POST':
+        article.delete()
+        return redirect('articles:index')
+    else:
+        return redirect('articles:detail', article.pk)
